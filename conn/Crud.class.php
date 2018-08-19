c2faf49f4d78ff1856467f023711e62df32f76b2
Bruno Nogueira <brunomg2016@hotmail.com>
2018-08-18 21:10:54 -0300

    adcionando o metodo para inserir dados no db

--- conn/Crud.class.php
+++ conn/Crud.class.php
@@ -7,6 +7,13 @@ class Crud {
     private $conexion;
     private $table;
     private $terms;
+    private $data;
+
+    public function create($table, array $data) {
+        $this->table = (string) $table;
+        $this->data = $data;
+        $this->prepare('create');
+    }
 
     public function read($table, $terms = NUll, $parseString = NULL) {
         $this->table = (string) $table ? $table : NULL;
@@ -14,9 +21,9 @@ class Crud {
         if(!empty($parseString)):
             $this->parseString = (string) $parseString ? $parseString : NULL;
             parse_str($parseString, $this->parseString);
-            $this->execute();
+            $this->prepare('read');
         else:
-            $this->execute();
+            $this->prepare('read');
         endif;
     }
 
@@ -25,9 +32,9 @@ class Crud {
         if(!empty($parseString)):
             $this->parseString = (string) $parseString ? $parseString : NULL;
             parse_str($parseString,$this->parseString);
-            $this->execute();
+            $this->prepare('read');
         else:
-            $this->execute();
+            $this->prepare('read');
         endif;
     }
 
@@ -38,53 +45,92 @@ class Crud {
         return $this->error;
     }
 
+    public function getId() {
+        return $this->data;
+    }
+
+    //PRIVATES METHODS
+
     private function getConn() {
         $this->conexion = Connection::getInstance();
         $this->conexion = $this->conexion->getConn();
         return $this->conexion;
     }
 
-    private function getSyntax(){
-        if(empty($this->query)):
-            $this->query = "SELECT * FROM {$this->table} {$this->terms}";
-        endif;
+    private function getSyntax($type){
+        switch ($type):
+            case 'read':
+                if(empty($this->query)):
+                    $this->query = "SELECT * FROM {$this->table} {$this->terms}";
+                endif;
+                break;
+
+            case 'create':
+                $this->parseString = '';
+                $this->terms = '';
+
+                foreach($this->data as $key => $values):
+                    $this->parseString .= "{$key}, ";
+                    $this->terms .= ":{$key}, ";
+                endforeach;
+
+                $this->parseString = substr($this->parseString, 0, -2);
+                $this->terms = substr($this->terms, 0, -2);
+                $this->query = "INSERT INTO {$this->table} ($this->parseString) VALUES ($this->terms)";
+                break;
+        endswitch;
     }
 
+    private function setBinds(array $dados) {
+        foreach($dados as $key => $value):
+            $this->query->bindValue(":{$key}",$value, (is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR));
+        endforeach;
+    }
 
     private function execute() {
+        $this->query->execute();
         
-        if(!empty($this->parseString)):
-            $this->getSyntax();
-            $this->query = $this->getConn()->prepare($this->query);
-
-            foreach($this->parseString as $key => $value):
-                $this->query->bindValue(":{$key}",$value, (is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR));
-            endforeach;
-            
-            $this->query->execute();
-            
-            if($this->query->rowCount() > 0):
-                $this->result = $this->query->fetchAll();
-                $this->error = FALSE;
-            else:
-                $this->result = NULL;
-                $this->error = TRUE;
-            endif;
-
+        if($this->query->rowCount() > 0):
+            $this->result = $this->query->fetchAll();
+            $this->error = FALSE;
         else:
-            $this->getSyntax();
-            $this->query = $this->getConn()->query($this->query);
-
-            if($this->query->rowCount() > 0):
-                $this->result = $this->query->fetchAll();
-                $this->error = FALSE;
-            else:
-                $this->result = NULL;
-                $this->error = TRUE;
-            endif;
-
+            $this->result = NULL;
+            $this->error = TRUE;
         endif;
-        
+
+        $this->data = $this->getConn()->lastInsertId();
     }
+
+    private function prepare($type) {
+        
+        switch($type):
+            case 'read':
+                if(!empty($this->parseString)):
+                    $this->getSyntax('read');
+                    $this->query = $this->getConn()->prepare($this->query);
+                    $this->setBinds($this->parseString);
+                    $this->execute();
+                else:
+                    $this->getSyntax('read');
+                    $this->query = $this->getConn()->query($this->query);
+
+                    if($this->query->rowCount() > 0):
+                        $this->result = $this->query->fetchAll();
+                        $this->error = FALSE;
+                    else:
+                        $this->result = NULL;
+                        $this->error = TRUE;
+                    endif;
+                endif;
+            break;
+            case 'create':
+                $this->getSyntax('create');
+                $this->query = $this->getConn()->prepare($this->query);
+                $this->setBinds($this->data);
+                $this->execute();
+            break;
+        endswitch;
+    }
+
 
 }
--- index.php
+++ index.php
@@ -5,9 +5,18 @@ require_once 'conn/Crud.class.php';
 $p1 = new Crud;
 //$p1->fullRead("SELECT * FROM usuarios");
 
-$p1->read('usuarios','where id = :id','id=1');
+$p1->read('carros');
 if(!$p1->getError()):
     echo'<pre>';
     var_dump($p1->getResult());
+    echo'</pre>';
+endif;
+echo '<hr>';
+$p2 = new Crud;
+$data = ["nome" => "I30"];
+//$p2->create('carros',$data);
+if(!$p2->getError()):
+    echo'<pre>';
+    var_dump($p2->getResult());
     echo'</pre>';
 endif;
